27c99c11cd96c41241f3595a9a018a793e8f57d5
Matias <m@tiasberge.com>
2018-09-30 21:02:52 +0200

    gta

--- bot.py
+++ bot.py
@@ -5,21 +5,19 @@ import random
 
 driver = webdriver.Chrome()
 
-username = 'berge'
-password = '50COdZhv1FF1'
+
 
 def site_login():
     driver.get('https://www.nordicmafia.org/login.php')
-    driver.find_element_by_name('username').send_keys(username)
-    driver.find_element_by_name('password').send_keys(password)
+    driver.find_element_by_name('username').send_keys('FlygendeNordmann')
+    driver.find_element_by_name('password').send_keys('')
     driver.find_element_by_name('login').click()
-    time.sleep(random.uniform(1, 2))
+    time.sleep(random.uniform(2, 3))
 
 def crime():
     randomNumber = random.randint(1, 5)
-    time.sleep(2)
     driver.find_element_by_link_text('Kriminalitet').click()
-    time.sleep(2)
+    time.sleep(random.uniform(1, 2))
     try:
         if randomNumber == 1:
             driver.find_element_by_id('rowid_table_select_krimaction0').click()
@@ -33,21 +31,25 @@ def crime():
             driver.find_element_by_id('rowid_table_select_krimaction4').click()
     except NoSuchElementException:
         None
+    print('Gjort kriminalitet')
+    time.sleep(random.uniform(1, 2))
 
 def blackmail():
     driver.find_element_by_link_text('Utpressing').click()
-    time.sleep(2)
+    time.sleep(random.uniform(1, 2))
     try:
         driver.find_element_by_id('sel_1').click()
         driver.find_element_by_name('submitBlackmail').click()
     except NoSuchElementException:
         None
+    print('Gjort utpressing')
+    time.sleep(random.uniform(2, 3))
 
-def gta():
+
+def carTheft():
     randomNumber = random.randint(1, 4)
-    time.sleep(2)
     driver.find_element_by_link_text('Biltyveri/Garasje').click()
-    time.sleep(2)
+    time.sleep(random.uniform(2, 3))
     try:
         if randomNumber == 1:
             driver.find_element_by_id('rowid_table_select_gtaaction0').click()
@@ -59,19 +61,40 @@ def gta():
             driver.find_element_by_id('rowid_table_select_gtaaction3').click()
     except NoSuchElementException:
         None
+    try:
+        driver.find_element_by_css_selector('background-color: #ff4c4c;').click()
+    except NoSuchElementException:
+        None
+    try:
+        driver.find_element_by_xpath('//*[@id="mainContent"]/div[2]/span/span[2]').click()
+        print('klikket på teksten')
+    except:
+        print('Fant ikkje fengselteksten')
+    print('Gjort biltyveri')
 
-def prison():
-    tmpText = (driver.find_element_by_class_name('bheader').text)
-    if tmpText == 'MÅ VENTE':
-        print('Venter...')
-        time.sleep(180)
+
+
 
 
 site_login()
-
-while 1:
-    time.sleep(0)
-    blackmail()
+while True:
     crime()
-    gta()
-    prison()
+    blackmail()
+    carTheft()
+    time.sleep(random.uniform(180, 200))
+    crime()
+    time.sleep(random.uniform(180, 200))
+    crime()
+    carTheft()
+    time.sleep(random.uniform(180, 200))
+    crime()
+    time.sleep(random.uniform(180, 200))
+    crime()
+    carTheft()
+    time.sleep(random.uniform(180, 200))
+    crime()
+    time.sleep(random.uniform(180, 200))
+
+
+
+
