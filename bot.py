0cdb87e616e7b2ff3740677349ad24b3c5979a65
Matias <m@tiasberge.com>
2018-09-30 20:37:05 +0200

    gta

--- bot.py
+++ bot.py
@@ -3,14 +3,15 @@ from selenium.common.exceptions import NoSuchElementException
 import time
 import random
 
-driver = webdriver.Safari()
+driver = webdriver.Chrome()
 
-
+username = 'berge'
+password = '50COdZhv1FF1'
 
 def site_login():
     driver.get('https://www.nordicmafia.org/login.php')
-    driver.find_element_by_name('username').send_keys('FlygendeNordmann')
-    driver.find_element_by_name('password').send_keys('Freak123123')
+    driver.find_element_by_name('username').send_keys(username)
+    driver.find_element_by_name('password').send_keys(password)
     driver.find_element_by_name('login').click()
     time.sleep(random.uniform(1, 2))
 
@@ -36,11 +37,41 @@ def crime():
 def blackmail():
     driver.find_element_by_link_text('Utpressing').click()
     time.sleep(2)
-    driver.find_element_by_id('sel_1').click()
-    driver.find_element_by_name('submitBlackmail').click()
+    try:
+        driver.find_element_by_id('sel_1').click()
+        driver.find_element_by_name('submitBlackmail').click()
+    except NoSuchElementException:
+        None
+
+def gta():
+    randomNumber = random.randint(1, 4)
+    time.sleep(2)
+    driver.find_element_by_link_text('Biltyveri/Garasje').click()
+    time.sleep(2)
+    try:
+        if randomNumber == 1:
+            driver.find_element_by_id('rowid_table_select_gtaaction0').click()
+        if randomNumber == 2:
+            driver.find_element_by_id('rowid_table_select_gtaaction1').click()
+        if randomNumber == 3:
+            driver.find_element_by_id('rowid_table_select_gtaaction2').click()
+        if randomNumber == 4:
+            driver.find_element_by_id('rowid_table_select_gtaaction3').click()
+    except NoSuchElementException:
+        None
+
+def prison():
+    tmpText = (driver.find_element_by_class_name('bheader').text)
+    if tmpText == 'MÅ VENTE':
+        print('Venter...')
+        time.sleep(180)
 
 
 site_login()
-blackmail()
-crime()
 
+while 1:
+    time.sleep(0)
+    blackmail()
+    crime()
+    gta()
+    prison()
